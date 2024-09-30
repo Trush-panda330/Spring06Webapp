0d67faed3c985430bb35420a06ca65bb9aee5137
uetak330 <uetak330@gmail.com>
2024-10-01 00:47:25 +0900

    登録の処理の追記

--- Spring06Webapp/src/main/java/uetak/repository/ShainRepositoryImpl.java
+++ Spring06Webapp/src/main/java/uetak/repository/ShainRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -39,7 +40,20 @@ public class ShainRepositoryImpl implements ShainRepository {
 
 	@Override
 	public void insertShain(Shain shain) {
-		// TODO 自動生成されたメソッド・スタブ
+		//SQL文の作成
+		final String sql = "insert into shain(id, name, gender, nen, address)"
+				+ "values(:id, :name, :gender, :nen, :address)";
+		
+		// パラメーターの作成
+		MapSqlParameterSource param = new MapSqlParameterSource();
+		param.addValue("id", shain.getId());
+		param.addValue("name", shain.getName());
+		param.addValue("gender", shain.getGender());
+		param.addValue("nen", shain.getNen());
+		param.addValue("address", shain.getAddress());
+		
+		//SQLの実行
+		jdbcTemplate.update(sql, param);
 
 	}
 
