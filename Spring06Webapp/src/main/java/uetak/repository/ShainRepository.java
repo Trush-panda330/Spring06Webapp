b374c6eb28a0a966d9ca7d7428ef251ab39e9c43
uetak330 <uetak330@gmail.com>
2024-09-30 21:16:46 +0900

    インデント修正

--- Spring06Webapp/src/main/java/uetak/repository/ShainRepository.java
+++ Spring06Webapp/src/main/java/uetak/repository/ShainRepository.java
@@ -5,19 +5,19 @@ import java.util.List;
 import uetak.entity.Shain;
 
 public interface ShainRepository {
-	
+
 	//全社員を取得
-	List<Shain>findAll();
-	
-    //社員IDから一件の社員情報を取得
+	List<Shain> findAll();
+
+	//社員IDから一件の社員情報を取得
 	Shain findByShainId(int shainId);
-	
+
 	//社員データの挿入
 	void insertShain(Shain shain);
-	
+
 	//社員更新
 	void updateShain(Shain shain);
-	
+
 	//社員削除
 	void deleteShain(Shain shain);
 
