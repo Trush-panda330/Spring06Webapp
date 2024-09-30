ac11e8ca5d9056e249a10d781d3f236ef530974e
uetak330 <uetak330@gmail.com>
2024-09-30 20:49:12 +0900

    初回コミット

---
+++ Spring06Webapp/src/main/java/uetak/Spring06WebappApplication.java
@@ -0,0 +1,13 @@
+package uetak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06WebappApplication.class, args);
+	}
+
+}
