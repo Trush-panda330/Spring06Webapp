b1fd2bbe0f6345b4d560a0b5e42be5cfa1c1b145
uetak330 <uetak330@gmail.com>
2024-10-01 10:28:40 +0900

    社員データ更新時のController層の処理

--- Spring06Webapp/src/main/java/uetak/controller/ShainController.java
+++ Spring06Webapp/src/main/java/uetak/controller/ShainController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model; //import 2つ候補がでるけどSpringの
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uetak.entity.Shain;
 import uetak.service.ShainService;
@@ -30,24 +31,52 @@ public class ShainController {
 		return "index";
 		//本来は return "/WEB-INF/index.jsp"だがapplication.propertiesにて設定を変更している
 	}
-	
+
 	@GetMapping("/insert")
 	public String insert() {
 		//JSPに転送
 		return "insert";
 	}
-	
+
 	@PostMapping("/insert")
 	public String insert(@ModelAttribute Shain request) {
 		//パラメータ値から社員作成
 		Shain shain = shainService.makeShain(request);
-		
+
 		//DBに挿入
 		shainService.insertShain(shain);
-		
+
 		//redirect
 		return "redirect:/index";
-		
+
+	}
+
+	@GetMapping("/update")
+	public String update(
+			@RequestParam("id") int id,
+			Model model) {
+
+		//対象データを取得
+		Shain shain = shainService.findByShainId(id);
+
+		// JSPに渡す
+		model.addAttribute("shain", shain);
+
+		// JSPに転送
+		return "update";
+	}
+
+	@PostMapping("/update")
+	public String update(@ModelAttribute Shain request) {
+
+		//パラメータ値から社員作成
+		Shain shain = shainService.makeShain(request);
+
+		//DB更新
+		shainService.updateShain(shain);
+
+		// リダイレクト
+		return "redirect:/index";
 	}
 
 }
